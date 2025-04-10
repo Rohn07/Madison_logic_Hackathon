ff36db805bf8399485f70a2205967a2a02f5124b
rbuddhdev <rbuddhdev@madisonlogic.com>
2025-04-10 14:12:28 +0530

    add: new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["904e-49-248-229-115.ngrok-free.app"],
+  },
 };
 
 export default nextConfig;
